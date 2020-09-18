ff751e88a2f137753ede0f3fdcc44e1327484aac
foxfabi <fd@fabforge.ch>
2020-09-18 13:07:21 -0400

    #new added course indicators

--- resources/views/admin/requirements/edit.blade.php
+++ resources/views/admin/requirements/edit.blade.php
@@ -24,4 +24,59 @@
 
     </b-card>
 
+    <b-card>
+        <template #header>{{__('t.views.admin.indicators.new')}}</template>
+
+        @component('components.form', ['route' => ['admin.indicators.store', ['course' => $course->id, 'requirement' => $requirement->id]]])
+            <input-text @forminput('content') label="{{__('t.models.indicator.content')}}" required autofocus></input-text>
+
+            {{-- <input-checkbox @forminput('mandatory', false) label="{{__('t.models.requirement.mandatory')}}"></input-checkbox> --}}
+
+
+            <button-submit label="{{__('t.global.add')}}">
+
+                @component('components.help-text', ['id' => 'indicatorsHelp', 'key' => 't.views.admin.indicators.what_are_indicators'])@endcomponent
+
+            </button-submit>
+
+        @endcomponent
+
+    </b-card>
+    <b-card>
+        <template #header>{{__('t.views.admin.indicators.existing', ['requirementName' => $requirement->content])}}</template>
+
+        @if (count($course->indicators))
+
+            @php
+                $fields = [
+                    __('t.models.indicator.content') => function(\App\Models\Indicator $indicator) { return $indicator->content; },
+                    // __('t.models.indicator.mandatory') => function(\App\Models\Requirement $requirement) { return $requirement->mandatory ? __('t.global.yes') : __('t.global.no'); },
+                    // __('t.models.indicator.num_observations') => function(\App\Models\Indikator $indicator) { return count($indicator->observations); },
+                ];
+                if ($course->archived) {
+                    unset($fields[__('t.models.indicator.num_observations')]);
+                }
+            @endphp
+            @component('components.responsive-table', [
+                'data' => $requirement->indicators,
+                'fields' => $fields,
+                'actions' => [
+                    'edit' => function(\App\Models\Indicator $indicator) use ($course) { return route('admin.indicators.edit', ['course' => $course->id, 'indicator' => $indicator->id]); },
+                    'delete' => function(\App\Models\Indicator $indicator) use ($course) { return [
+                        'text' => __('t.views.admin.indicators.really_delete') . ($course->archived ? '' : ' ' . trans_choice('t.views.admin.indicators.observations_on_indicator', $indicator->observations)),
+                        'route' => ['admin.indicators.delete', ['course' => $course->id, 'indicator' => $indicator->id]],
+                    ];},
+                ]
+            ])@endcomponent
+
+        @else
+
+            {{__('t.views.admin.indicators.no_indicators')}}
+
+            @component('components.help-text', ['id' => 'noIndicatorsHelp', 'key' => 't.views.admin.indicators.are_indicators_required'])@endcomponent
+
+        @endif
+
+    </b-card>
+
 @endsection
